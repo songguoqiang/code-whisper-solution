414364e897e19344fcc94fd794fbfae04dbdfc7b
songguoqiang <songguoq@gmail.com>
2018-11-03 12:17:44 +0800

    pass round 1

--- whisper.js
+++ whisper.js
@@ -1,3 +1,6 @@
-export default function(question){
-  return {end: question.start}
+export default function(question) {
+  let start = question.start;
+  let forwardSteps = (question.instructions.match(new RegExp("F", "g")) || [])
+    .length;
+  return { end: start + forwardSteps };
 }
